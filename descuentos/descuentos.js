5c26b9f1c46c92b4fe71a58b00f9b312a5ba7ade
FReyeSFT <fernandoreyezpinto@gmail.com>
2022-02-12 22:07:38 -0500

    termino la clase y el taller del día 12/02/2022

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,1 +1,41 @@
-alert("hpta");
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    
+    const precioNuevo = precioConDescuento;
+    return precioNuevo;
+}
+
+var cuponesDescuento = [
+    "FREYES",
+    "FREYESFT",
+    "BAGUIRA",
+    "PEDRO"
+];
+
+function calcularDescuento(){
+    var inputPrice = document.getElementById("input-price");
+    var priceValue = inputPrice.value;
+
+    var inputDiscount = document.getElementById("input-discount").value;
+    const calculo = document.getElementById("descuento-calculado");
+    switch (inputDiscount) {
+        case cuponesDescuento[0]:
+            inputDiscount = 30;
+            break;
+        case cuponesDescuento[1]:
+            inputDiscount = 60;
+            break;
+        case cuponesDescuento[2]:
+            inputDiscount = 10;
+            break;
+        case cuponesDescuento[3]:
+            inputDiscount = 5;
+            break;
+        default:
+            break;
+    }
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, inputDiscount);
+    calculo.innerText = `El precio con descuento es de: $${precioConDescuento}`;
+    console.log(priceValue, inputDiscount);
+}
